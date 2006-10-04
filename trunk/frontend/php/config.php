6e083fd7f306933f81b4d5ad129df01df882ba34
tivi <tivi@3ab9daf1-ec1c-0410-a6f7-b5b9ea6adccf>
2006-10-04 09:41:45 +0000

    Actualizado fichero de configuraciÃn de php

--- trunk/frontend/php/config.php
+++ trunk/frontend/php/config.php
@@ -4,7 +4,7 @@
  * Parámetros de configuración de la aplicación
  * 
  * @author Francisco Javier Ramos Álvarez
- * @version 1.3
+ * @version 1.1
  * @package php
  */
 	
@@ -15,15 +15,16 @@
 	define('DN_BASE', 'o=sadesi,o=empleados,o=juntadeandalucia,c=es');
 	
 	//paths de la aplicación
-	define('USERS_INI', '/var/www/pooler/trunk/frontend/other/users_repository.ini');
-	define('PATH_LOG', '/var/www/pooler/trunk/frontend/logs');
-	define('PATH_TEMP', '/var/www/pooler/trunk/frontend/tmp');
+	define('PATH_REPOSITORY', '/home/admin/guadalinex-flamenco');
+	define('USERS_INI', '/home/admin/pooler/trunk/frontend/other/users_repository.ini');
+	define('PATH_LOG', '/home/admin/pooler/trunk/frontend/logs');
+	define('PATH_TEMP', '/home/admin/pooler/trunk/frontend/tmp');
 	
 	//configuración para python
-	define('REPO_CONF', '/var/www/pooler/trunk/backend/conf/repo.conf');
-	define('ADD_PKG_PY', '/var/www/pooler/trunk/backend/addpkg.py');
-	define('MV_PKG_PY', '/var/www/pooler/trunk/backend/mvpkg.py');
-	define('RM_PKG_PY', '/var/www/pooler/trunk/backend/rmpkg.py');
+	define('REPO_CONF', '/home/admin/pooler/conf/repo.conf');
+	define('ADD_PKG_PY', '/home/admin/pooler/trunk/backend/addpkg.py');
+	define('MV_PKG_PY', '/home/admin/pooler/trunk/backend/mvpkg.py');
+	define('RM_PKG_PY', '/home/admin/pooler/trunk/backend/rmpkg.py');
 	
 	//tiempo máximo de ejecución de ciertos scripts susceptibles
 	//de superar el time out (se recomienda no decrementar)
@@ -33,7 +34,7 @@
 	define('PY_DEBUG', 1); //si -> 1, no -> 0
 	
 	//Indica si se muestran o no los warnigns de inicio
-	define('SHOW_WARNIGN', 1); //si -> 1, no -> 0
+	define('SHOW_WARNIGN', 0); //si -> 1, no -> 0
 	
 	//campos de filtrado, tanto de paquetes como de ficheros fuentes
 	$fieldsFilter = array(
